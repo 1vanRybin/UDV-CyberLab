22403481e6ff2763b38f9b52a08e73126fa94a4b
Ivan <117839645+1vanRybin@users.noreply.github.com>
2024-11-09 18:13:07 +0500

    connect dependences.

--- Api/TestService/Api/Program.cs
+++ Api/TestService/Api/Program.cs
@@ -1,25 +1,30 @@
+using ExampleCore.Swagger;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
+builder.Services.AddSwaggerStartUpBase();
+builder.Services.TryAddServices();
+builder.Services.TryAddInfrastucture(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    await context.Database.MigrateAsync();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapRazorPages();
+app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
---
+++ Api/TestService/Service/ServiceStartUp.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Service.interfaces;
+using WebApi.Services;
+
+public static class ServiceStartUp
+{
+    public static IServiceCollection TryAddServices(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddScoped<ITestService, TestsService>();
+
+        return serviceCollection;
+    }
+}
