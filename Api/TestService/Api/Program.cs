7468fc854a7e720dace251ed0b6f1aa1f058131c
Asrom11 <asrom.11@mail.ru>
2025-01-10 20:40:29 +0500

    add cors

--- Api/TestService/Api/Program.cs
+++ Api/TestService/Api/Program.cs
@@ -9,6 +9,18 @@ builder.Services.AddHttpClient();
 builder.Services.AddSwaggerStartUpBase();
 builder.Services.TryAddServices();
 builder.Services.TryAddInfrastucture(builder.Configuration);
+builder.Services.AddCors(cors =>
+
+    cors.AddDefaultPolicy(policy => policy
+        .WithOrigins("https://localhost:7023",
+            "http://90.156.168.7:8080",
+            "https://neolab.aydlioh.ru",
+            "http://localhost:5173",
+            "http://localhost:4173")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials()));
+
 builder.Services.AddControllers()
     .AddNewtonsoftJson(options =>
     {
