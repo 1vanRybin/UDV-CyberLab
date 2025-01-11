a98344a6af6e63ab2a94b20b14fd43776222fc0b
Ivan <117839645+1vanRybin@users.noreply.github.com>
2025-01-11 17:10:47 +0500

    fix sort.

--- Api/TestService/Domain/DTO/Questions/QuestionDto.cs
+++ Api/TestService/Domain/DTO/Questions/QuestionDto.cs
@@ -6,6 +6,7 @@ public abstract record QuestionBaseDto : IQuestionBase
 {
     public string Text { get; set; }
     public string Description { get; set; }
+    public int SortOrder { get; set; }
     public int Points { get; set; }
     public Guid TestId { get; set; }
 }
