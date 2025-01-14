39c4512685271fbeaeb337171582e1576baaa2c4
Ivan <117839645+1vanRybin@users.noreply.github.com>
2025-01-14 16:28:30 +0500

    add serialize.

--- Api/TestService/Domain/DTO/Questions/QuestionCompliance.cs
+++ Api/TestService/Domain/DTO/Questions/QuestionCompliance.cs
@@ -1,9 +1,12 @@
 ﻿
+using ExampleCore.Dal.Base;
+
 namespace Domain.DTO.Questions;
 
 public record QuestionComplianceDto : QuestionBaseDto
 {
     public Dictionary<string, string> Compliances { get; set; }
     public Dictionary<string,string> RightCompliances { get; set; }
-    public Dictionary<string, string[]> Variants { get; set; }
+    public string VariantsJson { get; set; }
+    public Dictionary<string, List<string>> Variants { get; set; }
 }
